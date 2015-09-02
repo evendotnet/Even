5ab9ec6f96bb272c43e6149d43574baa0c3808a2
Natan Vivo <nvivo64@gmail.com>
2015-09-02 08:45:41 -0300

    Removed data access library from core project.

--- Src/Even/EventStoreConfig.cs
+++ Src/Even/EventStoreConfig.cs
@@ -1,16 +1,7 @@
-﻿using DBHelpers;
-
-namespace Even
+﻿namespace Even
 {
     public class EventStoreConfig
     {
         public static EventStoreConfig Default { get; } = new EventStoreConfig();
-
-        //public string MySqlConnectionString { get; } = "Server=localhost; Database=eventstore; Uid=eventstore; Pwd=eventstore;";
-
-        //internal DBHelper GetHelper()
-        //{
-        //    return new DBHelper(MySql.Data.MySqlClient.MySqlClientFactory.Instance, MySqlConnectionString);
-        //}
     }
 }
