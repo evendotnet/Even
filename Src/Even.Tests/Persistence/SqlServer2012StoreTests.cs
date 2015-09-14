d245ba514f91d79133480954efd4b85ac1582473
Natan Vivo <nvivo64@gmail.com>
2015-09-14 14:17:46 -0300

    Added support for Sql Server 2012 and up.

---
+++ Src/Even.Tests/Persistence/SqlServer2012StoreTests.cs
@@ -0,0 +1,36 @@
+﻿using DBHelpers;
+using Even.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even.Tests.Persistence
+{
+    public class SqlServer2012StoreTests : EventStoreTests
+    {
+        protected override IEventStore InitializeStore()
+        {
+            var dbName = "even_test";
+            var cns = "Server=localhost\\sqlexpress; Trusted_Connection=True;";
+            var db = new DBHelper(SqlClientFactory.Instance, cns);
+
+            db.ExecuteNonQuery($"if db_id('{dbName}') is null create database {dbName};");
+
+            var store = new SqlServer2012Store(cns + "Database=" + dbName, null);
+            store.InitializeStore();
+
+            var truncateSql = $@"
+use {dbName};
+truncate table [{store.EventsTable}];
+truncate table [{store.ProjectionIndexTable}];
+truncate table [{store.ProjectionCheckpointTable}];";
+
+            db.ExecuteNonQueryAsync(truncateSql);
+
+            return store;
+        }
+    }
+}
