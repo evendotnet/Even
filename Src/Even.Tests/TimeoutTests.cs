8eeb2bd01607f0c59a31447e2254460b1ad93694
Natan Vivo <nvivo64@gmail.com>
2015-10-10 15:11:41 -0300

    Increased time for timeout tests.

--- Src/Even.Tests/TimeoutTests.cs
+++ Src/Even.Tests/TimeoutTests.cs
@@ -10,22 +10,22 @@ namespace Even.Tests
     public class TimeoutTests
     {
         [Theory]
-        [InlineData(50)]
         [InlineData(100)]
+        [InlineData(250)]
         [InlineData(500)]
         [InlineData(1000)]
         public async Task Timeout_does_not_expires_before_time(int milliseconds)
         {
             var timeout = Timeout.In(TimeSpan.FromMilliseconds(milliseconds));
             
-            await Task.Delay(milliseconds - 30);
+            await Task.Delay(milliseconds - 50);
 
             Assert.False(timeout.IsExpired);
         }
 
         [Theory]
-        [InlineData(50)]
         [InlineData(100)]
+        [InlineData(250)]
         [InlineData(500)]
         [InlineData(1000)]
         public async Task Timeout_is_expired_after_expected_time(int milliseconds)
