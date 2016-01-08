e347909c8232b84cab1a2ff491ee22375006541b
Damian Reeves <DamReev@gmail.com>
2016-01-08 10:21:26 -0500

    Creating CI build based on http://www.github.com/fsprojects/ProjectScaffold

--- Src/Even/Properties/AssemblyInfo.cs
+++ Src/Even/Properties/AssemblyInfo.cs
@@ -34,5 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.1.0")]
 [assembly: AssemblyFileVersion("0.0.1.0")]
-
-[assembly: InternalsVisibleTo("Even.Tests")]
