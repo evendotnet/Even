06eb3077cbb6abeaafda310d42518778f7e3deff
Damian Reeves <DamReev@gmail.com>
2016-07-29 01:52:55 -0400

    Updated to Akka version 1.1.1

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0-cake.1+152.Branch.feature/cake-v0.1.0.Sha.ba23cfb9c10203376d312d3038a989a75c49cf70")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyInformationalVersion("0.2.0-akka-upgrade.0+Branch.feature/akka-upgrade-v0.2.0.Sha.55a89037e60e8812cd25c778acb4c10c819d02d1")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
