22783eedffc0579ac220de81888df546f2b92ac8
Pablo Vivera <pvivera@datis.com>
2016-08-24 16:37:54 -0400

    Support for SQLite

---
+++ Test/Even.Tests/Persistence/SQLiteEventStoreTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.Common;
+using DBHelpers;
+using Even.Persistence.SQLite;
+
+namespace Even.Tests.Persistence
+{
+#if SQLITE
+    public
+#endif
+    class SQLiteEventStoreTests : EventStoreTests
+    {
+        private string ConnectionString = "Data Source=event_test.db;Version=3;New=True;";
+        protected override IEventStore CreateStore()
+        {
+            return new SQLiteStore(ConnectionString, true);
+        }
+
+        protected override void ResetStore()
+        {
+            var db = new DBHelper(DbProviderFactories.GetFactory("System.Data.SQLite"), ConnectionString);
+
+            var store = (SQLiteStore)Store;
+
+            var a = store.EventsTable;
+            var b = store.ProjectionIndexTable;
+            var c = store.ProjectionCheckpointTable;
+
+            db.ExecuteNonQuery($"DELETE FROM {a}; DELETE FROM SQLITE_SEQUENCE WHERE NAME='{a}'; DELETE FROM {b}; DELETE FROM SQLITE_SEQUENCE WHERE NAME='{b}'; DELETE FROM {c}; DELETE FROM SQLITE_SEQUENCE WHERE NAME='{c}';");
+        }
+    }
+}
